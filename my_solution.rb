b33e9c819ebf90b7d893eedc184e98250d377e15
Graham-Stein <graham@guavoaviation.co.uk>
2018-09-12 20:05:23 +0100

    Completed homework

--- my_solution.rb
+++ my_solution.rb
@@ -135,40 +135,73 @@ p "Erik's updated lott nos: #{users ["Erik"][:lottery_numbers]}"
 users ["Erik"][:home_town]="Edinburgh"
 p users ["Erik"][:home_town]
 # 9. Add a pet dog to Erik called "Fluffy"
-users ["Erik"][:pets][0] = {
-  :name => "Fluffy",
-  :species => "dog"
-  })
-  p
+users ["Erik"][:pets].push(
+                            name: "Fluffy",
+                            species: "dog"
+                          )
+  p users ["Erik"][:pets]
 # 10. Add another person to the users hash
-#
+users ["Rambo"] = {
+  :twitter => "BalboaR",
+  :lottery_numbers => [12, 45, 31, 42, 21, 46],
+  :home_town => "Hollywood",
+  :pets => [
+    {
+      :name => "balboa",
+      :species => "Boa Constrictor"
+    }
+
+  ]
+
+}
+
+p users["Rambo"]
 #
 # ## Exercise C
 #
 # ### Given the following data structure:
 #
 # ```ruby
-# united_kingdom = [
-#   {
-#     name: "Scotland",
-#     population: 5295000,
-#     capital: "Edinburgh"
-#   },
-#   {
-#     name: "Wales",
-#     population: 3063000,
-#     capital: "Swansea"
-#   },
-#   {
-#     name: "England",
-#     population: 53010000,
-#     capital: "London"
-#   }
-# ]
+united_kingdom = [
+  {
+    name: "Scotland",
+    population: 5295000,
+    capital: "Edinburgh"
+  },
+  {
+    name: "Wales",
+    population: 3063000,
+    capital: "Swansea"
+  },
+  {
+    name: "England",
+    population: 53010000,
+    capital: "London"
+  }
+]
 # ```
 # ### Complete these tasks:
 #
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
-# 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
+united_kingdom[1][:capital]="Cardiff"
+p united_kingdom
+# 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array
+# (The capital is Belfast, and the population is 1,811,000).
+united_kingdom.push(name: "Northern Ireland", population: 1811000, capital: "Belfast")
+p united_kingdom
 # 3. Use a loop to print the names of all the countries in the UK.
+
+
+  for country in united_kingdom
+    p country[:name]
+  end
+
+
+
 # 4. Use a loop to find the total population of the UK.
+  total_population = 0
+for country in united_kingdom
+
+  total_population += country[:population]
+end
+p total_population
